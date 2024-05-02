df69e30475264963bc31a385e5c889c43929759a
quannguyen2153 <122888400+quannguyen2153@users.noreply.github.com>
2024-05-02 11:34:17 +0700

    Handle error in translation process

--- vietnamworks/vnw_data_processor.py
+++ vietnamworks/vnw_data_processor.py
@@ -5,6 +5,7 @@ import json
 from googletrans import Translator
 
 import re
+import time
 from datetime import datetime, timedelta
 
 class VNWDataProcessor():
@@ -30,15 +31,45 @@ class VNWDataProcessor():
         
         self.save(df=merged_df, output_path=output_path)
         
-    def translateText(self, text, src_lng, dst_lng, retries=5):
-        for i in range(retries):
-            try:
-                if len(text.strip()) == 0 or self.translator.detect(text).lang == dst_lng:
-                    return text
+    def translateText(self, org_text, src_lng, dst_lng, retries=5, truncation_length=30):
+        def truncateText(text):
+            if '\n' in text[:truncation_length]:
+                truncated_index = text[:truncation_length].index('\n')
+                return text[:truncated_index].strip() + '...'
+            else:
+                if len(text) > truncation_length:
+                    return text[:truncation_length-3].strip() + '...'
+                else:
+                    return text.strip()
                 
-                return self.translator.translate(text, src=src_lng, dest=dst_lng).text
+        print('\"{:<{}}\"'.format(truncateText(org_text), truncation_length), end=' ')
+        
+        for i in range(retries-1):
+            time.sleep(2**i)
+            
+            try:
+                if len(org_text.strip()) == 0 or self.translator.detect(org_text).lang == dst_lng:
+                    print('{:^13} \"{:<{}}\"'.format('remains', truncateText(org_text), truncation_length))
+                    return org_text
+                                
+                translated_text = self.translator.translate(org_text, src=src_lng, dest=dst_lng).text
+                
+                print('{:^13} \"{:<{}}\"'.format('translated to', truncateText(translated_text), truncation_length))                
+                return translated_text
             except:
+                print('retrying', end=' ')
                 continue
+            
+        time.sleep(2**retries)
+        
+        if len(org_text.strip()) == 0 or self.translator.detect(org_text).lang == dst_lng:
+            print('{:^13} \"{:<{}}\"'.format('remains', truncateText(org_text), truncation_length))
+            return org_text
+                        
+        translated_text = self.translator.translate(org_text, src=src_lng, dest=dst_lng).text
+                
+        print('{:^13} \"{:<{}}\"'.format('translated to', truncateText(translated_text), truncation_length))                
+        return translated_text
     
     def translateTextList(self, text_list, src_lng, dst_lng):
         return [self.translateText(text, src_lng, dst_lng) for text in text_list]
@@ -84,10 +115,10 @@ if __name__ == '__main__':
     # vnw_data_processor.mergeJobSegments(job_segments_dir='vietnamworks/rawdata/jobinfo/segments',
     #                                 output_path='vietnamworks/rawdata/jobinfo/vnw_jobinfo_full.json')
     
-    df = pd.read_json('vietnamworks/rawdata/jobinfo/translated_jobinfo.json', encoding="utf-8")
+    df = pd.read_json('vietnamworks/rawdata/jobinfo/segments/vnw_jobinfo_500.json', encoding="utf-8")
     
-    # translated_df = vnw_data_processor.translateJobInfo(df, 'auto', 'en')
+    translated_df = vnw_data_processor.translateJobInfo(df, 'auto', 'en')    
+    vnw_data_processor.save(df=translated_df, output_path='vietnamworks/rawdata/jobinfo/translated_jobinfo_500.json')
     
-    # vnw_data_processor.save(df=translated_df, output_path='vietnamworks/rawdata/jobinfo/translated_jobinfo_500.json')
-    df = vnw_data_processor.calculateEndDate(jobinfo_df=df)
-    vnw_data_processor.save(df=df, output_path='vietnamworks/rawdata/jobinfo/preprocessed_jobinfo.json')
+    # df = vnw_data_processor.calculateEndDate(jobinfo_df=df)
+    # vnw_data_processor.save(df=df, output_path='vietnamworks/rawdata/jobinfo/preprocessed_jobinfo.json')
