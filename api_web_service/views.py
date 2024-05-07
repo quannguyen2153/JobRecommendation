f228571f31130d198f049621e28ce595b6a807d3
phathnv <hongocvinhphat@gmail.com>
2024-05-08 00:45:05 +0700

    refactor: Sign In, Sign up

--- api_web_service/views.py
+++ api_web_service/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.base import View
-from django.http import HttpResponse
+from django.http import JsonResponse, HttpResponse
 from django import forms
 import json
 
@@ -16,7 +16,6 @@ class SignUpView(View):
   '''
   def get(self, request): 
     return HttpResponse("GET method is not supported.", status=405)
-  
   class PostForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField()
@@ -25,45 +24,28 @@ class SignUpView(View):
   def post(self, request):
     # Get data from request
     try:
+      # Validate and extract request data
       data = json.loads(request.body)
-      
-      # Validate request data
       form = self.PostForm(data)
       if not form.is_valid():
         raise Exception(form.errors.as_data())
-      
-      # Extract data
-      email = data['email']
-      password = data['password']
-      username = data['username']
     except Exception as e:
-      return HttpResponse(json.dumps({"success": False, "message": str(e)}), content_type="application/json", status=400)
-    
+      return JsonResponse({"success": False, "message": str(e)}, status=400)
+
     # Register user
     try:
       # Call firebase API
-      result = firebase_client.auth().create_user_with_email_and_password(email=email, password=password)
-
+      result = firebase_client.auth().create_user_with_email_and_password(email=form.data['email'], password=form.data['password'])
       # Create user in database
-      user_data = {
+      user = UserSerializer(data={
         "uid": result['localId'],
-        "email": email,
-        "username": username,
-      }
-      user = UserSerializer(data=user_data).create()
+        "email": result['email'],
+        "username": form.data['username'],
+      }).create()
       UserManager.create(user)
     except Exception as e:
-      response = {
-        "success": False,
-        "message": str(e),
-      }
-      return HttpResponse(json.dumps(response), content_type="application/json", status=500)
-    
-    response = {
-      "success": True,
-      "message": "User registered successfully",
-    }
-    return HttpResponse(json.dumps(response), content_type="application/json", status=201)
+      return JsonResponse({"success": False, "message": str(e)}, status=500)
+    return JsonResponse({"success": True, "message": "User registered successfully",}, status=201)
   
 class SignInView(View):
   '''
@@ -79,36 +61,26 @@ class SignInView(View):
   def post(self, request):
     # Get data from request
     try:
+      # Validate and extract request data
       data = json.loads(request.body)
-
-      # Validate request data
       form = self.PostForm(data)
       if not form.is_valid():
         raise Exception(form.errors.as_data())
-      
-      # Extract data
-      email = data['email']
-      password = data['password']
     except Exception as e:
-      return HttpResponse(json.dumps({"success": False, "message": str(e)}), content_type="application/json", status=400)
+      return JsonResponse({"success": False, "message": str(e)}, status=400)
     
     # Sign in user
     try:
       # Call firebase API
-      result = firebase_client.auth().sign_in_with_email_and_password(email=email, password=password)
+      result = firebase_client.auth().sign_in_with_email_and_password(email=form.data['email'], password=form.data['password'])
     except Exception as e:
-      response = {
-        "success": False,
-        "message": str(e),
-      }
-      return HttpResponse(json.dumps(response), content_type="application/json", status=500)
+      return JsonResponse({"success": False, "message": str(e)}, status=500)
     
     user_data = UserSerializer(UserManager.get(result['localId'])).data
-    response = {
+    response = JsonResponse({
       "success": True,
       "message": "User signed in successfully",
       "data": user_data,
-    }
-    httpResponse = HttpResponse(json.dumps(response), content_type="application/json", status=200)
-    httpResponse.headers['Set-Cookie'] = f"idToken={result['idToken']}"
-    return httpResponse
+    }, status=200)
+    response.headers['Set-Cookie'] = f"idToken={result['idToken']};refreshToken={result['refreshToken']};"
+    return response
